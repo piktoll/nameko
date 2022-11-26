4fb28f5c9b8623d310a43c261af5705071f45367
piktoll <szrenka.viktor.daniel@gmail.com>
2022-11-26 15:02:53 +0100

    Update

--- assets/js/nameko.js
+++ assets/js/nameko.js
@@ -1,53 +1,68 @@
+class NameSet {
+    constructor(familyName, firstName, secondName) {
+        this.familyName = familyName;
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+}
+
+const hangulNames = new NameSet(["김", "이", "박", "정", "최", "조", "강", "장", "윤", "임", "신", "유", "한", "오", "서", "전"], ["감", "건", "경", "국", "규", "근", "금", "기", "길", "계", "관", "광", "나", "남", "노", "내", "다", "도", "돈", "동", "대", "령", "력", "래", "명", "무", "미", "민", "범", "병", "본", "봉", "상", "서", "석", "선", "성", "소", "수", "순", "승", "시", "신", "세", "아", "여", "연", "영", "오", "용", "우", "유", "육", "윤", "웅", "응", "은", "익", "인", "일", "완", "원", "예", "의", "장", "정", "조", "종", "주", "준", "중", "지", "진", "재", "차", "찬", "창", "천", "청", "춘", "충", "치", "채", "태", "택", "평", "필", "하", "학", "한", "헌", "혁", "현", "형", "호", "홍", "효", "훈", "흠", "해", "혜", "화", "휘", "희"], ["강", "걸", "결", "경", "구", "국", "군", "규", "균", "근", "기", "길", "관", "권", "나", "남", "녕", "내", "동", "득", "라", "란", "락", "렬", "룡", "리", "린", "림", "래", "망", "목", "문", "미", "민", "매", "복", "봉", "빈", "배", "삼", "상", "석", "선", "섭", "성", "송", "순", "숙", "수", "승", "식", "아", "양", "언", "억", "연", "엽", "영", "열", "옥", "용", "우", "운", "욱", "울", "윤", "애", "은", "응", "익", "인", "일", "완", "왕", "원", "의", "전", "정", "종", "주", "준", "중", "지", "직", "진", "재", "제", "찬", "철", "춘", "태", "택", "표", "하", "한", "헌", "혁", "현", "형", "협", "홍", "호", "훈", "혜", "화", "환", "회", "희"]);
+const romanizedNames = new NameSet(["Kim", "Lee", "Park", "Jeong", "Choi", "Cho", "Kang", "Jang", "Yoon", "Lim", "Shin", "Yoo", "Han", "Oh", "Seo", "Jeon"], ["Gam", "Geon", "Gyeong", "Guk", "Gyu", "Geun", "Geum", "Gi", "Gil", "Gye", "Gwan", "Gwang", "Na", "Nam", "No", "Nae", "Da", "Do", "Don", "Dong", "Dae", "Ryeong", "Ryeok", "Rae", "Myeong", "Mu", "Mi", "Min", "Beom", "Byeong", "Bon", "Bong", "Sang", "Seo", "Seok", "Seon", "Seong", "So", "Su", "Sun", "Seung", "Shi", "Shin", "Se", "Ah", "Yeo", "Yeon", "Yeong", "Oh", "Yong", "U", "Yu", "Yuk", "Yun", "Ung", "Eung", "Eun", "Ik", "In", "Il", "Wan", "Won", "Ye", "Eui", "Jang", "Jeong", "Jo", "Jong", "Ju", "Jun", "Jung", "Ji", "Jin", "Jae", "Cha", "Chan", "Chang", "Cheon", "Cheong", "Chun", "Chung", "Chi", "Chae", "Tae", "Taek", "Pyeong", "Pil", "Ha", "Hak", "Han", "Heon", "Hyeok", "Hyeon", "Hyeong", "Ho", "Hong", "Hyo", "Hun", "Heum", "Hae", "Hye", "Hwa", "Hwi", "Hui"], ["gang", "geol", "gyeol", "gyeong", "gu", "guk", "gun", "gyu", "gyun", "geun", "gi", "gil", "gwan", "gwon", "na", "nam", "nyeong", "nae", "dong", "deuk", "ra", "ran", "rak", "ryeol", "ryong", "ri", "rin", "rim", "rae", "mang", "mok", "mun", "mi", "min", "mae", "bok", "bong", "bin", "bae", "sam", "sang", "seok", "seon", "seop", "seong", "song", "sun", "suk", "su", "seung", "shik", "ah", "yang", "eon", "eok", "yeon", "yeop", "yeong", "yeol", "ok", "yong", "u", "un", "uk", "ul", "yun", "ae", "eun", "eung", "ik", "in", "il", "wan", "wang", "won", "ui", "jeon", "jeong", "jong", "ju", "jun", "jung", "ji", "jik", "jin", "jae", "je", "chan", "cheol", "chun", "tae", "taek", "pyo", "ha", "han", "heon", "hyeok", "hyeon", "hyeong", "hyeop", "hong", "ho", "hun", "hye", "hwa", "hwan", "hoi", "hui"]);
+
+/*
+    arrays containing Korean name elements with Korean hangul and romanization listed below
+*/
+
+const korean = [
+    ["김", "이", "박", "정", "최", "조", "강", "장", "윤", "임", "신", "유", "한", "오", "서", "전"],
+    ["감", "건", "경", "국", "규", "근", "금", "기", "길", "계", "관", "광", "나", "남", "노", "내", "다", "도", "돈", "동", "대", "령", "력", "래", "명", "무", "미", "민", "범", "병", "본", "봉", "상", "서", "석", "선", "성", "소", "수", "순", "승", "시", "신", "세", "아", "여", "연", "영", "오", "용", "우", "유", "육", "윤", "웅", "응", "은", "익", "인", "일", "완", "원", "예", "의", "장", "정", "조", "종", "주", "준", "중", "지", "진", "재", "차", "찬", "창", "천", "청", "춘", "충", "치", "채", "태", "택", "평", "필", "하", "학", "한", "헌", "혁", "현", "형", "호", "홍", "효", "훈", "흠", "해", "혜", "화", "휘", "희"],
+    ["강", "걸", "결", "경", "구", "국", "군", "규", "균", "근", "기", "길", "관", "권", "나", "남", "녕", "내", "동", "득", "라", "란", "락", "렬", "룡", "리", "린", "림", "래", "망", "목", "문", "미", "민", "매", "복", "봉", "빈", "배", "삼", "상", "석", "선", "섭", "성", "송", "순", "숙", "수", "승", "식", "아", "양", "언", "억", "연", "엽", "영", "열", "옥", "용", "우", "운", "욱", "울", "윤", "애", "은", "응", "익", "인", "일", "완", "왕", "원", "의", "전", "정", "종", "주", "준", "중", "지", "직", "진", "재", "제", "찬", "철", "춘", "태", "택", "표", "하", "한", "헌", "혁", "현", "형", "협", "홍", "호", "훈", "혜", "화", "환", "회", "희"]
+];
+
+const romanized = [
+    ["Kim", "Lee", "Park", "Jeong", "Choi", "Cho", "Kang", "Jang", "Yoon", "Lim", "Shin", "Yoo", "Han", "Oh", "Seo", "Jeon"],
+    ["Gam", "Geon", "Gyeong", "Guk", "Gyu", "Geun", "Geum", "Gi", "Gil", "Gye", "Gwan", "Gwang", "Na", "Nam", "No", "Nae", "Da", "Do", "Don", "Dong", "Dae", "Ryeong", "Ryeok", "Rae", "Myeong", "Mu", "Mi", "Min", "Beom", "Byeong", "Bon", "Bong", "Sang", "Seo", "Seok", "Seon", "Seong", "So", "Su", "Sun", "Seung", "Shi", "Shin", "Se", "Ah", "Yeo", "Yeon", "Yeong", "Oh", "Yong", "U", "Yu", "Yuk", "Yun", "Ung", "Eung", "Eun", "Ik", "In", "Il", "Wan", "Won", "Ye", "Eui", "Jang", "Jeong", "Jo", "Jong", "Ju", "Jun", "Jung", "Ji", "Jin", "Jae", "Cha", "Chan", "Chang", "Cheon", "Cheong", "Chun", "Chung", "Chi", "Chae", "Tae", "Taek", "Pyeong", "Pil", "Ha", "Hak", "Han", "Heon", "Hyeok", "Hyeon", "Hyeong", "Ho", "Hong", "Hyo", "Hun", "Heum", "Hae", "Hye", "Hwa", "Hwi", "Hui"],
+    ["gang", "geol", "gyeol", "gyeong", "gu", "guk", "gun", "gyu", "gyun", "geun", "gi", "gil", "gwan", "gwon", "na", "nam", "nyeong", "nae", "dong", "deuk", "ra", "ran", "rak", "ryeol", "ryong", "ri", "rin", "rim", "rae", "mang", "mok", "mun", "mi", "min", "mae", "bok", "bong", "bin", "bae", "sam", "sang", "seok", "seon", "seop", "seong", "song", "sun", "suk", "su", "seung", "shik", "ah", "yang", "eon", "eok", "yeon", "yeop", "yeong", "yeol", "ok", "yong", "u", "un", "uk", "ul", "yun", "ae", "eun", "eung", "ik", "in", "il", "wan", "wang", "won", "ui", "jeon", "jeong", "jong", "ju", "jun", "jung", "ji", "jik", "jin", "jae", "je", "chan", "cheol", "chun", "tae", "taek", "pyo", "ha", "han", "heon", "hyeok", "hyeon", "hyeong", "hyeop", "hong", "ho", "hun", "hye", "hwa", "hwan", "hoi", "hui"]
+];
+
 window.onload = function () {
     const formatter = new Intl.NumberFormat("en-US", {
         maximumFractionDigits: 0
     });
-    /*
-        arrays containing Korean name elements with Korean hangul and romanization listed below
-    */
 
-    const korean = [
-        ["김", "이", "박", "정", "최", "조", "강", "장", "윤", "임", "신", "유", "한", "오", "서", "전"],
-        ["감", "건", "경", "국", "규", "근", "금", "기", "길", "계", "관", "광", "나", "남", "노", "내", "다", "도", "돈", "동", "대", "령", "력", "래", "명", "무", "미", "민", "범", "병", "본", "봉", "상", "서", "석", "선", "성", "소", "수", "순", "승", "시", "신", "세", "아", "여", "연", "영", "오", "용", "우", "유", "육", "윤", "웅", "응", "은", "익", "인", "일", "완", "원", "예", "의", "장", "정", "조", "종", "주", "준", "중", "지", "진", "재", "차", "찬", "창", "천", "청", "춘", "충", "치", "채", "태", "택", "평", "필", "하", "학", "한", "헌", "혁", "현", "형", "호", "홍", "효", "훈", "흠", "해", "혜", "화", "휘", "희"],
-        ["강", "걸", "결", "경", "구", "국", "군", "규", "균", "근", "기", "길", "관", "권", "나", "남", "녕", "내", "동", "득", "라", "란", "락", "렬", "룡", "리", "린", "림", "래", "망", "목", "문", "미", "민", "매", "복", "봉", "빈", "배", "삼", "상", "석", "선", "섭", "성", "송", "순", "숙", "수", "승", "식", "아", "양", "언", "억", "연", "엽", "영", "열", "옥", "용", "우", "운", "욱", "울", "윤", "애", "은", "응", "익", "인", "일", "완", "왕", "원", "의", "전", "정", "종", "주", "준", "중", "지", "직", "진", "재", "제", "찬", "철", "춘", "태", "택", "표", "하", "한", "헌", "혁", "현", "형", "협", "홍", "호", "훈", "혜", "화", "환", "회", "희"]
-    ];
+    const combo = formatter.format(korean[0].length * korean[1].length * korean[2].length);
 
-    const romanized = [
-        ["Kim", "Lee", "Park", "Jeong", "Choi", "Cho", "Kang", "Jang", "Yoon", "Lim", "Shin", "Yoo", "Han", "Oh", "Seo", "Jeon"],
-        ["Gam", "Geon", "Gyeong", "Guk", "Gyu", "Geun", "Geum", "Gi", "Gil", "Gye", "Gwan", "Gwang", "Na", "Nam", "No", "Nae", "Da", "Do", "Don", "Dong", "Dae", "Ryeong", "Ryeok", "Rae", "Myeong", "Mu", "Mi", "Min", "Beom", "Byeong", "Bon", "Bong", "Sang", "Seo", "Seok", "Seon", "Seong", "So", "Su", "Sun", "Seung", "Shi", "Shin", "Se", "Ah", "Yeo", "Yeon", "Yeong", "Oh", "Yong", "U", "Yu", "Yuk", "Yun", "Ung", "Eung", "Eun", "Ik", "In", "Il", "Wan", "Won", "Ye", "Eui", "Jang", "Jeong", "Jo", "Jong", "Ju", "Jun", "Jung", "Ji", "Jin", "Jae", "Cha", "Chan", "Chang", "Cheon", "Cheong", "Chun", "Chung", "Chi", "Chae", "Tae", "Taek", "Pyeong", "Pil", "Ha", "Hak", "Han", "Heon", "Hyeok", "Hyeon", "Hyeong", "Ho", "Hong", "Hyo", "Hun", "Heum", "Hae", "Hye", "Hwa", "Hwi", "Hui"],
-        ["gang", "geol", "gyeol", "gyeong", "gu", "guk", "gun", "gyu", "gyun", "geun", "gi", "gil", "gwan", "gwon", "na", "nam", "nyeong", "nae", "dong", "deuk", "ra", "ran", "rak", "ryeol", "ryong", "ri", "rin", "rim", "rae", "mang", "mok", "mun", "mi", "min", "mae", "bok", "bong", "bin", "bae", "sam", "sang", "seok", "seon", "seop", "seong", "song", "sun", "suk", "su", "seung", "shik", "ah", "yang", "eon", "eok", "yeon", "yeop", "yeong", "yeol", "ok", "yong", "u", "un", "uk", "ul", "yun", "ae", "eun", "eung", "ik", "in", "il", "wan", "wang", "won", "ui", "jeon", "jeong", "jong", "ju", "jun", "jung", "ji", "jik", "jin", "jae", "je", "chan", "cheol", "chun", "tae", "taek", "pyo", "ha", "han", "heon", "hyeok", "hyeon", "hyeong", "hyeop", "hong", "ho", "hun", "hye", "hwa", "hwan", "hoi", "hui"]
-    ];
+    document.getElementById("generate-btn").addEventListener("click", generateName);
+    document.getElementById("mass-generate-btn").addEventListener("click", function () {
+        massGenerate(10);
+    });
+    document.getElementById("clear-list-btn").addEventListener("click", clearList);
+    document.getElementById("heading").innerText = `Korean Name Generator with ${combo} possible name combinations`;
+}
 
-    var echoName = document.getElementById("echoname");
-    var echoRomaja = document.getElementById("echoromaja");
-
-    var checkbox = document.getElementById("family");
-    var heading = document.getElementById("heading");
-    var list = document.getElementById("list");
-    let combo = formatter.format(korean[0].length * korean[1].length * korean[2].length);
-    heading.innerText = "Korean Name Generator with " + combo + " possible name combinations";
-    echoName.innerText = "Click Here!";
+function clearEcho() {
+    document.getElementById("echoname").innerText = "";
+    document.getElementById("echoromaja").innerText = "";
 }
 
 function generateName() {
-    echoName.innerText = "";
-    echoRomaja.innerText = "";
+    clearEcho();
 
-    if (checkbox.checked) {
+    if (document.getElementById("family").checked) {
         let pr = Math.floor(Math.random() * (korean[0].length - 1));
-        echoName.innerText = korean[0][pr];
-        echoRomaja.innerHTML = romanized[0][pr] + "&nbsp;";
+        document.getElementById("echoname").innerText = korean[0][pr];
+        document.getElementById("echoromaja").innerHTML = romanized[0][pr] + "&nbsp;";
     }
 
     let sr = Math.floor(Math.random() * (korean[1].length - 1));
     let tr = Math.floor(Math.random() * (korean[2].length - 1));
-    
-    echoName.innerHTML += korean[1][sr] + korean[2][tr];
-    echoRomaja.innerHTML += romanized[1][sr] + "-" + romanized[2][tr];
+
+    document.getElementById("echoname").innerHTML += korean[1][sr] + korean[2][tr];
+    document.getElementById("echoromaja").innerHTML += romanized[1][sr] + "-" + romanized[2][tr];
 }
 
 function clearList() {
-    list.innerHTML = "";
+    document.getElementById("list").innerHTML = "";
 }
 
 function massGenerate(n) {
@@ -58,10 +73,8 @@ function massGenerate(n) {
         let sr = Math.floor(Math.random() * (korean[1].length - 1));
         let tr = Math.floor(Math.random() * (korean[2].length - 1));
 
-        if (checkbox.checked) {
-            list.innerHTML += korean[0][pr] + korean[1][sr] + korean[2][tr] + "&nbsp;<span class=\"romaja\">(" + romanized[0][pr] + "&nbsp;" + romanized[1][sr] + "-" + romanized[2][tr] + ")" + "</span><br>";
-        } else {
-            list.innerHTML += korean[1][sr] + korean[2][tr] + "&nbsp;<span class=\"romaja\">(" + romanized[1][sr] + "-" + romanized[2][tr] + ")" + "</span><br>";
-        }
+        document.getElementById("family").checked
+            ? (document.getElementById("list").innerHTML += korean[0][pr] + korean[1][sr] + korean[2][tr] + "&nbsp;<span class=\"romaja\">(" + romanized[0][pr] + "&nbsp;" + romanized[1][sr] + "-" + romanized[2][tr] + ")" + "</span><br>")
+            : (document.getElementById("list").innerHTML += korean[1][sr] + korean[2][tr] + "&nbsp;<span class=\"romaja\">(" + romanized[1][sr] + "-" + romanized[2][tr] + ")" + "</span><br>");
     }
 }
--- nameko.php
+++ nameko.php
@@ -1,14 +1,17 @@
 <?php
-class Application {
+class Application
+{
     public $title;
     public $desc;
 
-    function __construct($title, $desc) {
+    function __construct($title, $desc)
+    {
         $this->title = $title;
         $this->desc = $desc;
     }
 
-    function printTitle() {
+    function printTitle()
+    {
         return "{$this->title} &ndash; {$this->desc}";
     }
 }
@@ -18,29 +21,36 @@ $app = new Application('nameko', 'Korean Name Generator');
 
 <!DOCTYPE html>
 <html lang="ko">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title><?php echo $app->printTitle(); ?></title>
-        <link rel="stylesheet" type="text/css" href="./assets/css/main.css">
-        <script type="text/javascript" src="./assets/js/nameko.js"></script>
-    </head>
-    <body>
-        <h1>name<span class="red">k</span><span class="blue">o</span></h1>
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title><?php echo $app->printTitle(); ?></title>
+    <link rel="stylesheet" type="text/css" href="./assets/css/main.css">
+    <script type="text/javascript" src="./assets/js/nameko.js"></script>
+</head>
+
+<body>
+    <header>
+        <h1 id="logo">name<span class="red">k</span><span class="blue">o</span></h1>
         <h2 id="heading"></h2>
         <h3>Click on the button below and create your very own Korean name!</h3>
-        <div class="familyname-selector"><input type="checkbox" name="family" id="family"><label for="family">Generate Family Name also</label></div>
+    </header>
+    <main>
+        <div class="familyname-selector"><input type="checkbox" name="family" id="family"><label for="family">Also generate Family Name</label></div>
         <button id="generate-btn" class="primary-btn">Generate!</button>
         <div id="echobox">
             <div id="echoname"></div>
             <div id="echoromaja" class="romaja"></div>
         </div>
         <div id="list"></div>
-        <button id="mass-generate" class="primary-btn">Mass Generate</button>
-        <button class="secondary-btn" onclick="clearList();">Clear List</button>
-        <footer>
-            <p><?php echo $app->printTitle(); ?></p>
-            <p>&copy;&nbsp;2022 Karhut Group. All Rights Reserved.</p>
-        </footer>
-    </body>
+        <button id="mass-generate-btn" class="primary-btn">Mass Generate</button>
+        <button id="clear-list-btn" class="secondary-btn">Clear List</button>
+    </main>
+    <footer>
+        <p><?php echo $app->printTitle(); ?></p>
+        <p>CC-BY-SA&nbsp;3.0 Karhut Group.</p>
+    </footer>
+</body>
+
 </html>
