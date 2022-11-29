0a8fe07cbfe3ab531b95b1b9d4d54ee25ac05ad0
Piktoll <114651661+piktoll@users.noreply.github.com>
2022-11-29 10:24:39 +0100

    Update nameko.php

--- nameko.php
+++ nameko.php
@@ -27,23 +27,24 @@ $app = new Application('nameko', 'Korean Name Generator');
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?php echo $app->printTitle(); ?></title>
     <link rel="stylesheet" type="text/css" href="./assets/css/main.css">
+    
     <script type="text/javascript" src="./assets/js/nameko.js"></script>
 </head>
 
 <body>
     <header>
         <h1 id="logo">name<span class="red">k</span><span class="blue">o</span></h1>
-        <h2 id="heading"></h2>
+        <h2 id="heading-el"></h2>
         <h3>Click on the button below and create your very own Korean name!</h3>
     </header>
     <main>
-        <div class="familyname-selector"><input type="checkbox" name="family" id="family"><label for="family">Also generate Family Name</label></div>
+        <div class="familyname-selector"><input type="checkbox" name="family-el" id="family-el"><label for="family-el">Also generate Family Name</label></div>
         <button id="generate-btn" class="primary-btn">Generate!</button>
         <div id="echobox">
-            <div id="echoname"></div>
-            <div id="echoromaja" class="romaja"></div>
+            <div id="name-el"></div>
+            <div id="romaja-el" class="romaja"></div>
         </div>
-        <div id="list"></div>
+        <div id="list-el"></div>
         <button id="mass-generate-btn" class="primary-btn">Mass Generate</button>
         <button id="clear-list-btn" class="secondary-btn">Clear List</button>
     </main>
@@ -52,5 +53,4 @@ $app = new Application('nameko', 'Korean Name Generator');
         <p>CC-BY-SA&nbsp;3.0 Karhut Group.</p>
     </footer>
 </body>
-
 </html>
